39537c769956d382d4a232fa8c1acce70d5addb5
zero-jianjia <zero_jianjia@163.com>
2016-07-28 22:54:32 +0800

    update

--- src/main/java/com/sina/scribe/Test.java
+++ src/main/java/com/sina/scribe/Test.java
@@ -1,20 +1,17 @@
 package com.sina.scribe;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by jianjia1 on 16/07/28.
  */
 public class Test {
-    private static final Logger LOG = LoggerFactory.getLogger(Test.class);
+//    private static final Logger LOG = LoggerFactory.getLogger(Test.class);
 
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             System.out.println(i);
-            LOG.info("hello");
+//            LOG.info("hello");
             try {
                 TimeUnit.MILLISECONDS.sleep(1000);
             } catch (InterruptedException e) {
